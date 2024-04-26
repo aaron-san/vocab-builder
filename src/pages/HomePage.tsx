ccfbb022ad3e8c607ec2bfa2dc62954eb6e4159f
fincoder <aaronhardy6@gmail.com>
2024-04-26 20:03:37 +0900

    build dist

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,15 @@
 const HomePage = () => {
-  return <section>HomePage</section>;
+  return (
+    <section>
+      HomePage
+      <a
+        href="javascript:void(0)"
+        className="inline-flex items-center justify-center border border-primary bg-primary px-7 py-3 text-center text-base font-medium text-white hover:border-[#1B44C8] hover:bg-[#1B44C8] active:border-[#1B44C8] active:bg-[#1B44C8] disabled:border-gray-3 disabled:bg-gray-3 disabled:text-dark-5"
+      >
+        Button
+      </a>
+    </section>
+  );
 };
 
 export default HomePage;
