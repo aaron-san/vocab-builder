05e589cc985c398e40ad2645faf1c52e3d74a1a5
fincoder <aaronhardy6@gmail.com>
2024-04-26 20:25:53 +0900

    build dist

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ const HomePage = () => {
     <section>
       HomePage
       <a
-        href="javascript:void(0)"
+        href=""
         className="inline-flex items-center justify-center border border-primary bg-primary px-7 py-3 text-center text-base font-medium text-white hover:border-[#1B44C8] hover:bg-[#1B44C8] active:border-[#1B44C8] active:bg-[#1B44C8] disabled:border-gray-3 disabled:bg-gray-3 disabled:text-dark-5"
       >
         Button
