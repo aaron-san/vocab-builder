5f5ec01114874db39aa37d723ca0f18f16d853ba
fincoder <aaronhardy6@gmail.com>
2024-04-26 21:22:17 +0900

    add card

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,31 @@
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 const HomePage = () => {
   return (
     <section>
       HomePage
-      <a
-        href=""
-        className="inline-flex items-center justify-center border border-primary bg-primary px-7 py-3 text-center text-base font-medium text-white hover:border-[#1B44C8] hover:bg-[#1B44C8] active:border-[#1B44C8] active:bg-[#1B44C8] disabled:border-gray-3 disabled:bg-gray-3 disabled:text-dark-5"
-      >
-        Button
-      </a>
+      <div className="card w-96 glass">
+        {/* <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="car!"
+          />
+        </figure> */}
+        <div className="p-0 card-body base-content">
+          <div className="w-fill h-[200px] bg-slate-200 opacity-60 rounded-t-xl"></div>
+          <div className="p-4">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="justify-end pt-4 card-actions">
+              <button className="btn btn-primary">
+                <FaArrowLeft />
+              </button>
+              <button className="btn btn-primary">
+                <FaArrowRight />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
