2758699a04b476a0226d21aa2f821f5ea932b7c4
fincoder <aaronhardy6@gmail.com>
2024-04-26 23:22:09 +0900

    style card

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 opacity-80 w-screen h-12 flex flex-row justify-between items-center px-4 mb-20">
-      <p className="text-blue-400 text-2xl bg-gradient-to-r from-blue-300 to-green-300 text-transparent bg-clip-text">
-        Vocab Builder
+    <header className="flex flex-row items-center justify-between w-screen h-12 px-4 mb-20 bg-gray-800 opacity-80">
+      <p className="text-2xl text-transparent text-blue-400 bg-gradient-to-r from-blue-300 to-green-300 bg-clip-text">
+        <Link to="/" className="text-inherit">
+          Vocab Builder
+        </Link>
       </p>
-      <ul className="flex w-40 gap-4 justify-end text-yellow-200">
+      <ul className="flex justify-end w-40 gap-4 text-yellow-200">
         <li>
           <Link to="/">Home</Link>
         </li>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,15 @@
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight, FaArrowLeft, FaEye } from "react-icons/fa";
+// import * as vocab_EL from "../";
+import vocab_EL from "../../vocab_EL.json";
+import { useState } from "react";
+
+const vc_EL = vocab_EL.vocab_EL;
 const HomePage = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [show, setShow] = useState(false);
+
   return (
     <section>
-      HomePage
       <div className="card w-96 glass">
         {/* <figure>
           <img
@@ -10,12 +17,34 @@ const HomePage = () => {
             alt="car!"
           />
         </figure> */}
-        <div className="p-0 card-body base-content">
-          <div className="w-fill h-[200px] bg-slate-200 opacity-60 rounded-t-xl"></div>
+        <div className="base-content">
+          <div className="w-fill h-[200px] bg-slate-100 opacity-80 rounded-t-xl flex flex-col justify-center items-center p-4 text-slate-700 text-4xl gap-4">
+            <div className="text-6xl">{vc_EL[0].英語}</div>
+
+            <div
+              className={
+                isMounted
+                  ? "transition-all opacity-100 scale-100 duration-500 delay-100 text-red-800"
+                  : "transition-all opacity-0 text-blue-800"
+              }
+              onAnimationEnd={() => {
+                if (!isMounted) setShow(false);
+              }}
+            >
+              {vc_EL[0].意味}
+            </div>
+          </div>
           <div className="p-4">
-            <h2 className="card-title">Life hack</h2>
-            <p>How to park your car at your garage?</p>
             <div className="justify-end pt-4 card-actions">
+              <button
+                className="mr-8 border btn-ghost btn border-1 border-slate-100"
+                onClick={() => {
+                  setIsMounted(!isMounted);
+                  if (!show) setShow(true); //We should Render our Div
+                }}
+              >
+                <FaEye className="w-[40px]" />
+              </button>
               <button className="btn btn-primary">
                 <FaArrowLeft />
               </button>
