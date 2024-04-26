6328d52db8b023f1596d7b9079b577d40ea5325a
fincoder <aaronhardy6@gmail.com>
2024-04-26 15:47:20 +0900

    update header

--- src/Template.tsx
+++ src/Template.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 const Template = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="flex flex-col justify-start items-center w-screen h-screen">
+    <main className="flex flex-col justify-start items-center w-screen h-screen bg-gradient-to-r from-slate-700 to-slate-600">
       <Header />
       {children}
     </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 opacity-80 w-screen h-12 flex flex-row justify-between items-center px-4">
-      <span>Vocab Builder</span>
-      <ul className="flex w-40 gap-4 justify-end">
+    <header className="bg-gray-800 opacity-80 w-screen h-12 flex flex-row justify-between items-center px-4 mb-20">
+      <p className="text-blue-400 text-2xl bg-gradient-to-r from-blue-300 to-green-300 text-transparent bg-clip-text">
+        Vocab Builder
+      </p>
+      <ul className="flex w-40 gap-4 justify-end text-yellow-200">
         <li>
           <Link to="/">Home</Link>
         </li>
